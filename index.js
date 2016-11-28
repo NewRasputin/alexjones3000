3c72e38f39f49bea347930e5c35a19c758e2e2ac
Ras <contact@rasputin.io>
2016-11-28 15:53:42 -0800

    Tweet sentence

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs')
 var MarkovChain = require('markovchain')
+var CronJob = require('cron').CronJob
 
+var T = require('./twit.js')
 var realJones = fs.readFileSync('./realJones.txt').toString()
 
 var words = realJones.split(' ')
@@ -11,6 +13,8 @@ function word () {
 	return words[Math.floor(Math.random()*words.length)]
 }
 
-setInterval(function () {
-	console.log(fakeJones.start(word()).end(17).process())
-}, 1000);
+new CronJob('00 00 */4 * * *', function() {
+	T.post('statuses/update', { status: fakeJones.start(word()).end(17).process() }, function(err, data, response) {
+		console.log(data)
+	})
+}, null, true, 'America/Los_Angeles');
